da5eca893e0133ebdb9aa826bd725a695db1f0c0
kim suna <108721070+ssunakimm@users.noreply.github.com>
2022-09-20 21:54:13 +0900

    0920 수정

---
+++ port/vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import "./assets/css/style.css"
+
+export default {  
+  name: 'App',
+  data(){
+    return {
+
+    }
+  },
+}
+</script>
